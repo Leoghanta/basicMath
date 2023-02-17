49a6c8d038dffa943e3a6f87b382e401df653461
Jimi Wilson <t.wilson@dundeeandangus.ac.uk>
2023-02-17 09:44:51 +0000

    Add project files.

---
+++ BasicMath.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+	public class BasicMath
+	{
+		/// <summary>
+		/// Add two numbers together
+		/// </summary>
+		/// <param name="num1">Integer to add</param>
+		/// <param name="num2">Integer to add</param>
+		/// <returns></returns>
+		public double Add (int num1, int num2)
+		{
+			return num1 + num2;
+		}
+
+		/// <summary>
+		/// Subtract num2 from num1
+		/// </summary>
+		/// <param name="num1">original int</param>
+		/// <param name="num2">int to subtract</param>
+		/// <returns></returns>
+		public double Subtract (int num1, int num2)
+		{
+			return num1 - num2;
+		}
+
+		/// <summary>
+		/// Multiply two numbers together
+		/// </summary>
+		/// <param name="num1">Int to multiply</param>
+		/// <param name="num2">Int to multiply</param>
+		/// <returns></returns>
+		public double Multiply (int num1, int num2)
+		{
+			return num1 * num2;
+		}
+
+		/// <summary>
+		/// Divide numerator by demonminator
+		/// </summary>
+		/// <param name="numerator">Top number of division as int</param>
+		/// <param name="denominator">divisor number as int</param>
+		/// <returns></returns>
+		public double Divide (int numerator, int denominator)
+		{
+			return numerator / denominator;
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp15
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			BasicMath basicMath = new BasicMath();
+			Console.WriteLine("Basic Math Examples");
+			Console.WriteLine($"5 + 7 = {basicMath.Add(5, 7)}");
+			Console.WriteLine($"8 - 4 = {basicMath.Subtract(8, 4)}");
+			Console.WriteLine($"7 * 6 = {basicMath.Multiply(7, 6)}");
+			Console.WriteLine($"9 / 3 = {basicMath.Divide(9, 3)}");
+
+		}
+	}
+}
